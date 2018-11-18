df45b7fc9707b756ec3daf1dd5e317c925cc8423
Anderson <anderson@softecsul.com>
2018-11-18 13:54:07 -0300

    Adiconado o form4

---
+++ TesteBranch/TesteBranch/Form4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteBranch
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "criado o formulário 4 na BRANCH 4";
+        }
+    }
+}
